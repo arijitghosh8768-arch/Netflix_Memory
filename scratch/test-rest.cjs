30846adcdf3acd2c2ca4b493769be50db541f5b4
arijitghosh8768-arch <arijitghosh8768@gmail.com>
2026-09-25 23:15:49 +0530

    remove compromised connection scripts

--- scratch/test-rest.cjs
+++
@@ -1,8 +0,0 @@
-﻿const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://ruxinzqymhlypxivyjbp.supabase.co', 'sb_publishable_5N4xWqmUoUphXKT881A25g_g1kLwqyO');
-
-async function test() {
-  const { data, error } = await supabase.from('couples').select('*');
-  console.log('Couples fetch:', error ? error.message : data);
-}
-test();
